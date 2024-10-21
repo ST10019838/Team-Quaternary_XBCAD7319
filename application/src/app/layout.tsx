1bba6a97a4abba85e4b0357ce4d808c5dcbb7c91
DJDare <djdare2003@gmail.com>
2024-10-21 13:25:16 +0200

    feat: added initial auth

--- application/src/app/layout.tsx
+++ application/src/app/layout.tsx
@@ -3,6 +3,15 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Providers from './providers'
 
+import {
+  ClerkProvider,
+  SignIn,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,10 +25,18 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <SignedOut>
+            <SignIn routing="hash" />
+          </SignedOut>
+          <SignedIn>
+            <UserButton showName />
+            <Providers>{children}</Providers>
+          </SignedIn>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
