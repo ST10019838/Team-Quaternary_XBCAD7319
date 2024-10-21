6b0a7365695059e73d9c3de0bb2a4067062935a1
DJDare <djdare2003@gmail.com>
2024-10-21 15:54:09 +0200

    feat: added column selector that will enable dynamic column searching

--- application/src/app/admin/page.tsx
+++ application/src/app/admin/page.tsx
@@ -14,62 +14,62 @@ import UsersTable from '@/components/ui/users-table'
 
 export default function page() {
   return (
-    <Tabs defaultValue="users" className="w-[400px]">
-      <TabsList>
-        <TabsTrigger value="users">Users</TabsTrigger>
-        <TabsTrigger value="horses">Horses</TabsTrigger>
-        <TabsTrigger value="addresses">Addresses</TabsTrigger>
-        <TabsTrigger value="contact_details">Contact Details</TabsTrigger>
-        <TabsTrigger value="payment_details">Payment Details</TabsTrigger>
-      </TabsList>
-      <TabsContent value="users">
-        {/* Insert Users Table Componenet Here */}
-        <UsersTable />
-      </TabsContent>
-      <TabsContent value="horses">
-        {/* Insert Horses Table Componenet Here */}
-        <HorsesTable />
-      </TabsContent>
-      <TabsContent value="addresses">
-        {/* Insert Addresses Table Componenet Here */}
-        <AddressesTable />
-      </TabsContent>
-      <TabsContent value="contact_details">
-        {/* Insert Contact Details Table Componenet Here */}
-        <ContactDetailsTable />
-      </TabsContent>
-      <TabsContent value="payment_details">
-        {/* Insert Payment Details Table Componenet Here */}
-        <PaymentDetailsTable />
-      </TabsContent>
-    </Tabs>
+    // <Tabs defaultValue="users" className="w-[400px]">
+    //   <TabsList>
+    //     <TabsTrigger value="users">Users</TabsTrigger>
+    //     <TabsTrigger value="horses">Horses</TabsTrigger>
+    //     <TabsTrigger value="addresses">Addresses</TabsTrigger>
+    //     <TabsTrigger value="contact_details">Contact Details</TabsTrigger>
+    //     <TabsTrigger value="payment_details">Payment Details</TabsTrigger>
+    //   </TabsList>
+    //   <TabsContent value="users">
+    //     {/* Insert Users Table Componenet Here */}
+    //     <UsersTable />
+    //   </TabsContent>
+    //   <TabsContent value="horses">
+    //     {/* Insert Horses Table Componenet Here */}
+    //     <HorsesTable />
+    //   </TabsContent>
+    //   <TabsContent value="addresses">
+    //     {/* Insert Addresses Table Componenet Here */}
+    //     <AddressesTable />
+    //   </TabsContent>
+    //   <TabsContent value="contact_details">
+    //     {/* Insert Contact Details Table Componenet Here */}
+    //     <ContactDetailsTable />
+    //   </TabsContent>
+    //   <TabsContent value="payment_details">
+    //     {/* Insert Payment Details Table Componenet Here */}
+    //     <PaymentDetailsTable />
+    //   </TabsContent>
+    // </Tabs>
 
-    // <div className="container mx-auto py-10">
-    //   <DataTable columns={columns} data={payments} />
-    // </div>
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={payments} />
+    </div>
   )
 }
 
 // Test data for table
-// type Payment = {
-//   id: string
-//   amount: number
-//   status: 'pending' | 'processing' | 'success' | 'failed'
-//   email: string
-// }
+type Payment = {
+  id: string
+  amount: number
+  status: 'pending' | 'processing' | 'success' | 'failed'
+  email: string
+}
 
-// export const payments: Payment[] = [
-//   {
-//     id: '728ed52f',
-//     amount: 100,
-//     status: 'pending',
-//     email: 'm@example.com',
-//   },
-//   {
-//     id: '489e1d42',
-//     amount: 125,
-//     status: 'processing',
-//     email: 'example@gmail.com',
-//   },
-//   // ...
-// ]
+export const payments: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com',
+  },
+  {
+    id: '489e1d42',
+    amount: 125,
+    status: 'processing',
+    email: 'example@gmail.com',
+  },
+  // ...
+]
